b1df9de39373bf0a8b4d697672dbb84df0f75ab0
Mark Learst <learst@me.com>
2023-08-27 02:56:47 -0400

    Fixed a list of lingering issues.
    - CSS bugs fixed causing pseudo elements to render incorrectly
    - Fixed favicon (temp fix, need to address cookie
    - Clean up React components
    - Updated JS Libs

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,41 +1,32 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { v4 as uuidv4 } from 'uuid';
+import React from "react";
+import PropTypes from "prop-types";
+import { v4 as uuidv4 } from "uuid";
 
-const tags = (tags) => (
-  tags
-    .split(',')
-    .map(tag => <span key={uuidv4()}>{tag}</span>)
+const tags = (tags) =>
+  tags.split(",").map((tag) => <span key={uuidv4()}>{tag}</span>);
+
+const Tile = ({ href, heading, subHeading, image }) => (
+  <li>
+    <a
+      href={href}
+      rel="noopener noreferrer"
+      target="_blank"
+      onClick={(event) => event.preventDefault()}
+    >
+      <h3>
+        <div>{heading}</div>
+        <section>{!!subHeading && tags(subHeading)}</section>
+      </h3>
+    </a>
+    <img src={image} alt={heading} />
+  </li>
 );
-
-const Tile = ({
-  href,
-  heading,
-  subHeading,
-  image,
-}) => (
-    <li>
-      <a
-        href={href}
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        <h3>
-          <div>{heading}</div>
-          <section>
-            {!!subHeading && tags(subHeading)}
-          </section>
-        </h3>
-      </a>
-      <img src={image} alt={heading} />
-    </li>
-  )
 
 Tile.propsTypes = {
   href: PropTypes.string.isRequired,
   heading: PropTypes.string.isRequired,
   subHeading: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-}
+};
 
-export default Tile
+export default Tile;
